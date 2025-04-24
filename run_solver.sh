780f09dc2de33cb323e14a9d803cfe45c411fcce
Joao1397 <joseadasilvar13@gmail.com>
2025-04-24 00:40:41 -0400

    Third Commit

--- run_solver.sh
+++ run_solver.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
 foamCleanTutorials
-
-# foamCleanPolyMesh
-
 blockMesh | tee log.blockMesh
-
 surfaceFeatures | tee log.surfaceFeatures
-
-snappyHexMesh | tee log.snappyHexMesh
-
-# snappyHexMesh –overwrite
-
-checkMesh -writeSets -latestTime | tee log.checkmesh
-
+decomposePar
+mpirun -np 2 snappyHexMesh -parallel | tee log.snappyHexMesh
+mpirun -np 2 checkMesh -latestTime -parallel | tee log.checkMesh
+reconstructPar -latestTime
 paraFoam -builtin
 
 # cp 3/polyMesh/* constant/polyMesh
@@ -21,27 +14,3 @@ paraFoam -builtin
 # rm -rf 2
 # rm -rf 3
 # checkMesh -latestTime
-
-#!/bin/bash
-
-# foamCleanTutorials
-# rm -r 0 > /dev/null 2>&1
-# blockMesh | tee log.blockMesh
-# surfaceFeatures | tee log.surfaceFeatures
-
-# decomposePar
-
-# mpirun -np 4 snappyHexMesh -parallel | tee log.snappyHexMesh
-
-#Clean dictionary with no regions but with cellZone
-#mpirun -np 4 snappyHexMesh -dict system/snappyHexMeshDict_v1 -parallel | tee log.snappyHexMesh
-
-#Clean dictionary with no regions and no cellZone
-#mpirun -np 4 snappyHexMesh -dict system/snappyHexMeshDict_v2 -parallel | tee log.snappyHexMesh
-
-
-# mpirun -np 4 checkMesh -latestTime -parallel| tee log.checkMesh
-
-#reconstructParMesh -latestTime
-
-#paraFoam -builtin
